5773fb150b4d93230165fd5926ba66f5b0b1125f
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-03-20 20:47:42 +0100

    Filling components with data

--- angular-project/src/app/tabs/tabs.component.ts
+++ angular-project/src/app/tabs/tabs.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    {name: 'Luke Skywalker', side: ''},
+    {name: 'Darth Vader', side: ''}
+  ];
+
+  chsosenList = 'all';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onChoose(side){
+    this.chsosenList = side;
+  }
+  getCharacters(){
+    if(this.chsosenList ='all'){
+      return this.chsosenList.slice();
+    }
+    return this.characters.filter((char) =>{
+      return char.side === this.chsosenList;
+    })
   }
 
 }
