29d1fc4e3e58a29b791dc2051dd2be3e3e4bf794
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-03-23 18:35:42 +0100

    code review: creating service

--- angular-project/src/app/item/item.component.ts
+++ angular-project/src/app/item/item.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
+  providers: [StarWarsService]
 })
 export class ItemComponent implements OnInit {
   @Input() character;
   @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
-  constructor() { }
+  swService: StarWarsService;
+
+  constructor(swService : StarWarsService) {
+    this.swService = swService;
+  }
 
   ngOnInit(): void {
   }
   onAssign(side){
     //this.character.side= side;
-    this.sideAssigned.emit({name: this.character.name, side:side})
+    this.swService.onSideChosen({name: this.character.name, side:side})
   }
 }
