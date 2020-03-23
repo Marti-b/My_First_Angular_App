86ec45639194bc10777278d723b1c02ad45c6a0a
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-03-23 19:04:26 +0100

    Code review: cleaning

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { StarWarsService } from './star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StarWarsService]
 })
 export class AppComponent {
   title = 'angular-project';
